54b687b610f9056988964ecfb84e05e9569a5b2f
sizen90 <69831199+sizen90@users.noreply.github.com>
2021-02-19 21:25:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
-app.use('/', express.static('./web'));
+app.use('/', express.static('.'));
 app.listen(PORT, ()=>console.log('app listen on port '+PORT))

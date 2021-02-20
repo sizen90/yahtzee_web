987f8674b677595bc480efd6fbe75a882bcaa357
sizen <sizencloud@gmail.com>
2021-02-20 12:56:47 +0000

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,7 +15,7 @@ const RESOURCES = {
 "index.html": "03447911c65b9352560f700f5760975c",
 "/": "03447911c65b9352560f700f5760975c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "78ef671cddb97ca28a1e85eec96bb48d",
+"main.dart.js": "9adc63c1e4f7e1fee2ce73ab9ce16ea0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
